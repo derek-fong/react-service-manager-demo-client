8a28d16830ad0f62d489443a5aaf4851c13042a4
Derek Fong <derek.fong@health.wa.gov.au>
2019-04-19 23:01:09 +0800

    feat(home): add HomeContainer component

---
+++ src/home/HomeContainer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Container } from 'react-bootstrap';
+
+function HomeContainer() {
+  return (
+    <Container>
+      <h1>Home Container</h1>
+    </Container>
+  );
+}
+
+export default HomeContainer;
