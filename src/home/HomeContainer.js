15028ea220406e94b100019e9e455d679aff7176
Derek Fong <derek.fong@health.wa.gov.au>
2019-04-20 19:16:59 +0800

    feat(home): add jumbotron

--- src/home/HomeContainer.js
+++ src/home/HomeContainer.js
@@ -1,34 +1,51 @@
-import React from 'react';
-import { Col, Container, Row } from 'react-bootstrap';
+import React, { Fragment } from 'react';
+import { Col, Container, Jumbotron, Row } from 'react-bootstrap';
 
 import RequestCounter from './RequestCounter.js';
 
 function HomeContainer() {
   return (
-    <Container>
-      <Row>
-        <Col>
-          <RequestCounter
-            headerText="Open Requests"
-            linkRef="/requests"
-            linkText="View Requests"
-            requestStatusTypes={[
-              'REGISTERED',
-              'IN_PROGRESS',
-              'PENDING_CUSTOMER'
-            ]}
-          />
-        </Col>
-        <Col>
-          <RequestCounter
-            headerText="Completed Requests"
-            linkRef="/requests/create"
-            linkText="Create a New Request"
-            requestStatusTypes={['FULFILLED', 'CLOSED']}
-          />
-        </Col>
-      </Row>
-    </Container>
+    <Fragment>
+      <Jumbotron>
+        <h1>React Service Manager</h1>
+        <p>
+          A simple request management application, written in{' '}
+          <a
+            title="React"
+            href="https://reactjs.org/"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            React
+          </a>
+          .
+        </p>
+      </Jumbotron>
+      <Container>
+        <Row>
+          <Col>
+            <RequestCounter
+              headerText="Open Requests"
+              linkRef="/requests"
+              linkText="View Requests"
+              requestStatusTypes={[
+                'REGISTERED',
+                'IN_PROGRESS',
+                'PENDING_CUSTOMER'
+              ]}
+            />
+          </Col>
+          <Col>
+            <RequestCounter
+              headerText="Completed Requests"
+              linkRef="/requests/create"
+              linkText="Create a New Request"
+              requestStatusTypes={['FULFILLED', 'CLOSED']}
+            />
+          </Col>
+        </Row>
+      </Container>
+    </Fragment>
   );
 }
 
