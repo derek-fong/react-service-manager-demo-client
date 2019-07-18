4b436deacd2e722a1ac4c81ba465726536b9ab61
Derek Fong <derek.fong@health.wa.gov.au>
2019-07-18 16:31:15 +0800

    feat(requests): add requests counter GraphQL query

--- src/home/RequestCounter.js
+++ src/home/RequestCounter.js
@@ -1,63 +1,74 @@
+import gql from 'graphql-tag';
+import has from 'has';
 import PropTypes from 'prop-types';
-import React, { useState, useEffect } from 'react';
+import React from 'react';
+import { Query } from 'react-apollo';
 import { Alert, Card, Col, Row, Spinner } from 'react-bootstrap';
 
-import fakeApi from '../fake-api/FakeApi.js';
-
-function RequestCounter({ headerText, linkRef, linkText, requestStatusTypes }) {
-  const [count, setCount] = useState(undefined);
-  const [error, setError] = useState(undefined);
-  const [isLoading, setIsLoading] = useState(false);
-  let contentElement;
-
-  useEffect(() => {
-    (async () => {
-      setError(undefined);
-      setIsLoading(true);
-
-      try {
-        setCount(await fakeApi.getRequestCountAsync(requestStatusTypes));
-      } catch (e) {
-        setError(e);
-        setCount(undefined);
-      }
-
-      setIsLoading(false);
-    })();
-  }, []);
-
-  if (isLoading) {
-    contentElement = (
-      <Row>
-        <Col>
-          <Spinner animation="grow" role="status" variant="primary">
-            <span className="sr-only">Loading...</span>
-          </Spinner>
-          <span>Loading...</span>
-        </Col>
-      </Row>
-    );
-  } else if (error) {
-    contentElement = (
-      <Alert variant="danger">
-        <Alert.Heading>Failed to Load Request Count</Alert.Heading>
-        <pre>{error.toString()}</pre>
-      </Alert>
-    );
-  } else {
-    contentElement = <h1>{count}</h1>;
-  }
+const RequestCounter = ({
+  headerText,
+  linkRef,
+  linkText,
+  requestStatusTypes
+}) => {
+  const queryRequestsCount = gql`
+    query RequestsByStatusCount($status: [Status!]!) {
+      requestsByStatusCount(status: $status)
+    }
+  `;
 
   return (
-    <Card className="text-center">
-      <Card.Header>{headerText}</Card.Header>
-      <Card.Body>{contentElement}</Card.Body>
-      <Card.Footer>
-        <Card.Link href={linkRef}>{linkText}</Card.Link>
-      </Card.Footer>
-    </Card>
+    <Query
+      query={queryRequestsCount}
+      variables={{ status: requestStatusTypes }}
+      fetchPolicy="cache-and-network"
+    >
+      {({ data, error, loading }) => {
+        let contentElement;
+
+        if (loading) {
+          contentElement = (
+            <Row>
+              <Col>
+                <Spinner animation="grow" role="status" variant="primary">
+                  <span className="sr-only">Loading...</span>
+                </Spinner>
+                <span>Loading...</span>
+              </Col>
+            </Row>
+          );
+        } else {
+          if (error) {
+            contentElement = (
+              <Alert variant="danger">
+                <Alert.Heading>Failed to Load Request Count</Alert.Heading>
+                <pre>{error.toString()}</pre>
+              </Alert>
+            );
+          } else {
+            contentElement = (
+              <h1>
+                {data && has(data, 'requestsByStatusCount')
+                  ? data.requestsByStatusCount
+                  : 'N/A'}
+              </h1>
+            );
+          }
+        }
+
+        return (
+          <Card className="text-center">
+            <Card.Header>{headerText}</Card.Header>
+            <Card.Body>{contentElement}</Card.Body>
+            <Card.Footer>
+              <Card.Link href={linkRef}>{linkText}</Card.Link>
+            </Card.Footer>
+          </Card>
+        );
+      }}
+    </Query>
   );
-}
+};
 
 RequestCounter.propTypes = {
   headerText: PropTypes.string.isRequired,
